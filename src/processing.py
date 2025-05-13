bae08a597ccb599f8c6a9fb576e5255fea5ce83e
Vladislav Cherniavskii <ledcher93@gmail.com>
2025-05-13 16:15:29 +0600

    Implementing the sort_by_date function and adding new tests to the main file

--- src/processing.py
+++ src/processing.py
@@ -6,5 +6,10 @@ def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> List[Dic
     return [operation for operation in operations if operation.get('state') == state]
 
 
-def sort_by_date():
-    pass
+def sort_by_date(operations: List[Dict], sorting_key: bool = True) -> List[Dict]:
+    """
+    Сортировка даты по убыванию|возрастанию
+    По умолчанию - с последних дат до новых
+    """
+
+    return sorted(operations, key=lambda operation: operation.get('date', ''), reverse=sorting_key)
