a8c87ea2e489ba6ced11068c322699d173ec3909
Vladislav Cherniavskii <ledcher93@gmail.com>
2025-05-14 10:21:10 +0600

    Correcting the docstring in the processing module

--- src/processing.py
+++ src/processing.py
@@ -2,15 +2,23 @@ from typing import Dict, List
 
 
 def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    """Сортировка операций по ключу"""
+    """
+    Отфильтровывает список операций по значению ключа "state".
+
+    Функция возвращает новый список словарей, в котором
+    присутствуют только те операции, у которых поле 'state'
+    совпадает с переданным значением.
+    """
 
     return [operation for operation in operations if operation.get('state') == state]
 
 
 def sort_by_date(operations: List[Dict], sorting_key: bool = True) -> List[Dict]:
     """
-    Сортировка даты по убыванию|возрастанию
-    По умолчанию - с последних дат до новых
+    Сортирует список операций по полю "date".
+
+    По умолчанию сортирует от самых новых к самым старым (sorting_key=True).
+    Можно передать sorting_key=False, чтобы получить порядок от старых к новым.
     """
 
     return sorted(operations, key=lambda operation: operation.get('date', ''), reverse=sorting_key)
