2250c231e7a79ed5073aeb7b8c563101c05728bc
Vladislav Cherniavskii <ledcher93@gmail.com>
2025-05-14 10:35:42 +0600

    Even more precise docstrings in the processing module

--- src/processing.py
+++ src/processing.py
@@ -8,17 +8,37 @@ def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> List[Dic
     Функция возвращает новый список словарей, в котором
     присутствуют только те операции, у которых поле 'state'
     совпадает с переданным значением.
+
+    Параметры:
+    operations : List[Dict]
+        Принимает список словарей, в котором каждый словарь это денежная операция.
+    state : str
+        Значение поля 'state', по которому производится фильтрация.
+        По умолчанию выбираются выполненные операции - "EXECUTED".
+
+    Возвращает:
+    List[Dict]
+        Новый список операций, отфильтрованный по указанному состоянию.
     """
 
     return [operation for operation in operations if operation.get('state') == state]
 
 
-def sort_by_date(operations: List[Dict], sorting_key: bool = True) -> List[Dict]:
+def sort_by_date(operations: List[Dict], reverse_: bool = True) -> List[Dict]:
     """
     Сортирует список операций по полю "date".
 
-    По умолчанию сортирует от самых новых к самым старым (sorting_key=True).
-    Можно передать sorting_key=False, чтобы получить порядок от старых к новым.
+    Параметры:
+
+    operations : List[Dict]
+        Принимает список словарей, в котором каждый словарь это денежная операция.
+    reverse_: bool = True
+        По умолчанию сортирует от самых новых к самым старым (reverse_=True).
+    Можно передать reverse_=False, чтобы получить порядок от старых к новым.
+
+    Возвращает:
+    List[Dict]
+        Новый список операций, отсортированный по дате.
     """
 
-    return sorted(operations, key=lambda operation: operation.get('date', ''), reverse=sorting_key)
+    return sorted(operations, key=lambda operation: operation.get('date', ''), reverse=reverse_)
